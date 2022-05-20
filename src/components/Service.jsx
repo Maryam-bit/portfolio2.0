bd7f83032a9a6da90143f2190e4beb21c8576856
maryam.noor.cowlar <maryam.noor.cowlar@gmail.com>
2022-05-20 12:07:22 -0700

    fixed styles of services card

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,18 +5,13 @@ const Service = ({ service }) => {
     return (
         <>
             <div className="card-body p-3 text-center">
-                {/* <div className="row mb-3"> */}
-                    {/* <div className="col-md-2"> */}
-                        <img className='service-img' src={require(`../assets/images/${service.image}.png`)} alt="" />
-                    {/* </div> */}
-                    {/* <div className="col-md-10"> */}
-                        <h6 className='service-name'>{service.name}</h6>
-                    {/* </div> */}
-                {/* </div> */}
-                
+                <img className='service-img' src={require(`../assets/images/${service.image}.png`)} alt="" />
+                <h6 className='service-name'>{service.name}</h6>
+                <div>
                     {service.tag.map((tag, index) => (
                         <button key={index} className="service-tag">{tag}</button>
                     ))}
+                </div>
             </div>
         </>
     )
