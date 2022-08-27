a2546e9e4951a9b656722c604926a4d52c6a0412
Maryam-bit <rajputmaryam5555@gmail.com>
2022-08-27 13:41:18 -0700

    removed dash

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
       <div className='home'>
         <h3 className='user-name'>{user.about.name}</h3>
-        <p className='breakpoint'>___________________</p>
+        {/* <p className='breakpoint'>___________________</p> */}
         <h6 className='user-profession'>{user.about.profession}</h6>
         <span className='user-description'>{user.about.description}</span>
       </div>
